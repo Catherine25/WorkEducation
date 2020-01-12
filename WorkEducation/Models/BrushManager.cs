1edb7c29da79d252333985c9830b4246f8fe307e
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-01-12 22:31:04 +0200

    Add BrushManager.cs

---
+++ WorkEducation/Models/BrushManager.cs
@@ -0,0 +1,20 @@
+﻿using SkiaSharp;
+using System.Collections.Generic;
+
+namespace WorkEducation.Models
+{
+	public class BrushManager
+	{
+		private ImageAnalyzer _imageAnalyzer;
+		private ColorTree _colorTree;
+
+		public BrushManager(SKPixmap image)
+		{
+			_imageAnalyzer = new ImageAnalyzer(image);
+
+			_colorTree = _imageAnalyzer.SortColors();
+		}
+
+		public List<Pixel> GetBrushes() => _colorTree.CurrentBrushSet;
+	}
+}
