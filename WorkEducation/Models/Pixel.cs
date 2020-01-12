403ecfbe31f899983252c20281eb21a01b152aac
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-01-12 22:30:10 +0200

    Add Pixel.cs

---
+++ WorkEducation/Models/Pixel.cs
@@ -0,0 +1,19 @@
+﻿using SkiaSharp;
+
+namespace WorkEducation.Models
+{
+	public class Pixel
+	{
+		public int _x;
+		public int _y;
+		
+		public SKColor _color;
+
+		public Pixel(int x, int y, SKColor color)
+		{
+			_x = x;
+			_y = y;
+			_color = color;
+		}
+	}
+}
