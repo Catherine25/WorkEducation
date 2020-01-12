db7c5067f868126a78d5f67b090046ddad734206
Catherine25 <CatherineSolodoukhina@gmail.com>
2020-01-12 22:30:51 +0200

    Add SKColorExtension.cs

---
+++ WorkEducation/Extensions/GetLightestFromSKColor.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkEducation.Extensions
+{
+	public static class SKColorExtension
+	{
+		public static float GetLightestFromSKColor(this SkiaSharp.SKColor color)
+		{
+			color.ToHsl(out float h1, out float s1, out float l1);
+
+			return l1;
+		}
+	}
+}
